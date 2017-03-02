13b19d03989a1b0ef4100dfdebeaeb39451a2c7e
Jean Marcos <jean_marcos27@hotmail.com>
2017-03-02 20:47:59 -0300

    Change laravel version to 5.4.

---
+++ app/Http/Controllers/FannController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class FannController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Realiza o treinamento da rede neural
+     */
+    public function train()
+    {
+        die('train');
+    }
+
+    /**
+     * Realiza o teste da rede neural treinada
+     */
+    public function test()
+    {
+        die('test');
+    }
+}

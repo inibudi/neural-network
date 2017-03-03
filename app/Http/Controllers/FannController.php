ee73105d5f8b61ffea0400cb36af59fcd67685f8
Jean Marcos <jean_marcos27@hotmail.com>
2017-03-02 21:26:57 -0300

    First implementing for neural network.

--- app/Http/Controllers/FannController.php
+++ app/Http/Controllers/FannController.php
@@ -20,9 +20,25 @@ class FannController extends Controller
      */
     public function train()
     {
+        $num_input = 2;
+        $num_output = 1;
+        $num_layers = 3;
+        $num_neurons_hidden = 3;
+        $desired_error = 0.001;
+        $max_epochs = 500000;
+        $epochs_between_reports = 1000;
+        $ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
+        if ($ann) {
+            fann_set_activation_function_hidden($ann, FANN_SIGMOID_SYMMETRIC);
+            fann_set_activation_function_output($ann, FANN_SIGMOID_SYMMETRIC);
 
-        
-        die('train');
+            $filename = storage_path("app/public/xor.data");
+            if (fann_train_on_file($ann, $filename, $max_epochs, $epochs_between_reports, $desired_error))
+                print('xor trained.<br>' . PHP_EOL);
+            if (fann_save($ann, storage_path("app/public/xor_float.net")))
+                print('xor_float.net saved.<br><a href="simple_test.php">Test</a>' . PHP_EOL);
+            fann_destroy($ann);
+        }
     }
 
     /**
@@ -30,6 +46,17 @@ class FannController extends Controller
      */
     public function test()
     {
-        die('test');
+        $train_file = storage_path("app/public/xor_float.net");
+        if (!is_file($train_file))
+            die("The file xor_float.net has not been created! Please run simple_train.php to generate it" . PHP_EOL);
+        $ann = fann_create_from_file($train_file);
+        if ($ann) {
+            $input = array(-1, 1);
+            $calc_out = fann_run($ann, $input);
+            printf("xor test (%f,%f) -> %f\n", $input[0], $input[1], $calc_out[0]);
+            fann_destroy($ann);
+        } else {
+            die("Invalid file format" . PHP_EOL);
+        }
     }
 }
